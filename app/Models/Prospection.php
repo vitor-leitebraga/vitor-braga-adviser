80ce10df37b5be17e84fe0ad97941fc1f2cd820b
Vitor Braga <vitor.braga@x-team.com>
2024-03-10 16:16:39 -0300

    Adding some relations to the model

--- app/Models/Prospection.php
+++ app/Models/Prospection.php
@@ -4,10 +4,21 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class Prospection extends Model
 {
     use HasFactory;
 
 	protected $guarded = ["id"];
+
+	public function consumer(): HasOne
+	{
+		return $this->hasOne(Consumer::class);
+	}
+
+	public function clientAddress(): HasOne
+	{
+		return $this->hasOne(ClientAddress::class);
+	}
 }
